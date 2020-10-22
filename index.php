544ce22f039e593443a71d55ddf0f7743c48b86b
FreddieMwau <37485156+FreddieMwau@users.noreply.github.com>
2020-10-22 20:29:17 +0300

    complete website except contact section

---
+++ index.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device - scale, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Carlos - Photographer</title>
+</head>
+<body>
+
+<!--Preloader Start-->
+<div class="preloader">
+    <div class="loader">
+    </div>
+</div>
+<!--Preloader ENd-->
+
+<!--Header Start-->
+<header class="header">
+    <div class="container">
+        <div class="row justify-content-between align-items-center">
+            <div class="brand-name">
+                <a href="index.html">Carlos.</a>
+            </div>
+            <div class="nav-toggle">
+                <span></span>
+            </div>
+            <nav class="nav">
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#work">Work</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+<!--Header End-->
+
+<!--Home Section Start-->
+<section class="home-section" id="home">
+    <!--shape-->
+    <div class="shape-01">
+    </div>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="home-content">
+                <h4>I'm Carlos Graphics</h4>
+                <h1>Photographer</h1>
+            </div>
+        </div>
+    </div>
+    <!--scroll down img-->
+    <a href="#about" class="scroll-down">
+        <img src="down.svg" alt="scroll Down">
+    </a>
+</section>
+<!--Home Section End-->
+
+<!--About Section Start-->
+<section class="about-section" id="about">
+    <div class="container">
+        <div class="row">
+            <div class="about-img">
+                <div class="img-box">
+                    <div class="shape-02"></div>
+                    <img src="about.png" alt="about me">
+                </div>
+            </div>
+            <div class="about-content">
+                <div class="row">
+                    <div class="section-title">
+                        <h1>About Me</h1>
+                    </div>
+                </div>
+                <p>Photographs have the incredible power to make me feel happy, excited, reflective, proud,
+                    thankful, peaceful, and an abundance of love. ... I put myself in others shoes all the time because I treat others how
+                    I'd like to be treated and I love relating to people. 3. I get so excited for the moments when all the stars align.</p>
+                <div class="stats">
+                    <div class="row">
+                        <div class="stat-box">
+                            <h2>15</h2>
+                            <h5>Awards Won</h5>
+                        </div>
+                        <div class="stat-box">
+                            <h2>50</h2>
+                            <h5>Projects Done</h5>
+                        </div>
+                        <div class="stat-box">
+                            <h2>45</h2>
+                            <h5>Happy Clients</h5>
+                        </div>
+                        <div class="stat-box">
+                            <h2>5000</h2>
+                            <h5>All Shoots</h5>
+                        </div>
+                    </div>
+                </div>
+                <!--Contact Btn-->
+                <a href="#contact" class="btn btn-01">Contact Me</a>
+            </div>
+        </div>
+    </div>
+</section>
+<!--About Section End-->
+
+<!--Services Section Start-->
+<section class="service-section" id="services">
+    <div class="container">
+        <div class="row">
+            <div class="section-title">
+                <h1>What I Do ?</h1>
+            </div>
+        </div>
+        <div class="row">
+            <!--service items start-->
+            <div class="service-item">
+                <div class="service-item-inner">
+                    <img src="images/small/wedding.png" alt="service">
+                    <div class="overlay">
+                        <h4>Wedding Photography</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="service-item">
+                <div class="service-item-inner">
+                    <img src="images/small/portrait.png" alt="service">
+                    <div class="overlay">
+                        <h4>Portrait Photography</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="service-item">
+                <div class="service-item-inner">
+                    <img src="images/small/outdoor.png" alt="service">
+                    <div class="overlay">
+                        <h4>Outdoor Photography</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="service-item">
+                <div class="service-item-inner">
+                    <img src="images/small/kids1.png" alt="service">
+                    <div class="overlay">
+                        <h4>Kids Photography</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="service-item">
+                <div class="service-item-inner">
+                    <img src="images/small/skits.png" alt="service">
+                    <div class="overlay">
+                        <h4>Skits Photography</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="service-item">
+                <div class="service-item-inner">
+                    <img src="images/small/fashion.png" alt="service">
+                    <div class="overlay">
+                        <h4>Fashion Photography</h4>
+                    </div>
+                </div>
+            </div>
+            <!--service items end-->
+        </div>
+    </div>
+</section>
+<!--Services Section End-->
+
+<!--Work Section Start-->
+<section class="work-section" id="work">
+    <div class="container">
+        <div class="row">
+            <div class="section-title">
+                <h1>My Works</h1>
+            </div>
+        </div>
+        <div class="row">
+            <!--Work item start-->
+            <div class="work-item">
+                <div class="work-item-inner">
+                    <img src="images/small/fashion.png" data-large="images/large/fashion.png" alt="work">
+                    <div class="overlay">
+                        <h4>Fashion Photography</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="work-item">
+                <div class="work-item-inner">
+                    <img src="images/small/wedding.png" data-large="images/large/wedding.png" alt="work">
+                    <div class="overlay">
+                        <h4>Wedding Photography</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="work-item">
+                <div class="work-item-inner">
+                    <img src="images/small/outdoor.png" data-large="images/large/jlo.png" alt="work">
+                    <div class="overlay">
+                        <h4>Outdoor Photography</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="work-item">
+                <div class="work-item-inner">
+                    <img src="images/small/portrait.png" data-large="images/large/portrait.png" alt="work">
+                    <div class="overlay">
+                        <h4>Portrait Photography</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="work-item">
+                <div class="work-item-inner">
+                    <img src="images/small/kids1.png" data-large="images/large/kid.png" alt="work">
+                    <div class="overlay">
+                        <h4>Kids Photography</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="work-item">
+                <div class="work-item-inner">
+                    <img src="images/small/skits.png" data-large="images/large/skit.png" alt="work">
+                    <div class="overlay">
+                        <h4>Skits Photography</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="work-item">
+                <div class="work-item-inner">
+                    <img src="images/small/outdoor1.png" data-large="images/large/outdoor.png" alt="work">
+                    <div class="overlay">
+                        <h4>Outdoor Photography</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="work-item">
+                <div class="work-item-inner">
+                    <img src="images/small/portrait1.png" data-large="images/large/portrait1.png" alt="work">
+                    <div class="overlay">
+                        <h4>Portrait Photography</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="work-item">
+                <div class="work-item-inner">
+                    <img src="images/small/outdoor2.png" data-large="images/large/crush.png" alt="work">
+                    <div class="overlay">
+                        <h4>Fashion Photography</h4>
+                    </div>
+                </div>
+            </div>
+            <!--Work item end-->
+        </div>
+    </div>
+</section>
+<!--Work Section End-->
+<!--lightbox start-->
+<div class="lightbox">
+    <div class="img-box">
+        <div class="lightbox-close">&times;</div>
+        <img src="images/large/wedding.png" class="lightbox-img" alt="img">
+        <div class="lightbox-caption">
+            <div class="lightbox-category"></div>
+            <div class="lightbox-counter"></div>
+        </div>
+        <div class="lightbox-controls">
+            <div class="prev">
+                <img src="previous.png" alt="prev">
+            </div>
+            <div class="next">
+                <img src="next.png" alt="next">
+            </div>
+        </div>
+    </div>
+</div>
+<!--lightbox end-->
+
+<!--Contact section start-->
+<section class="contact-section" id="contact">
+    <div class="container">
+        <div class="row">
+            <div class="contact-img">
+                <div class="img-box">
+                    <div class="section-title">
+                        <h1>Contact Me</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="contact-form">
+                <form class="form">
+                    <div class="row">
+                        <div class="left input-group">
+                            <label>Name</label>
+                            <input type="text" placeholder="John Doe" name="name" class="input-control">
+                        </div>
+                        <div class="right input-group">
+                            <label>Email or Phone</label>
+                            <input type="email" name="email" placeholder="johndoe@gmail.com" class="input-control">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-group">
+                            <label>Message</label>
+                            <textarea class="input-control" name="message" placeholder="Your Message Here......"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-btn">
+                        <button type="button" class="btn btn-01">Send Message</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <div class="contact-info">
+                <div class="row">
+                    <!--info item start-->
+                    <div class="info-item">
+                        <h5>Address</h5>
+                        <p>Nairobi Kenya</p>
+                    </div>
+                    <div class="info-item">
+                        <h5>Phone</h5>
+                        <p>+254 714 258 494</p>
+                    </div>
+                    <div class="info-item">
+                        <h5>Email</h5>
+                        <p>carlosgraphics@gmail.com</p>
+                    </div>
+                    <!--info item end-->
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="social-links">
+                <a href="#" title="instagram"><img src="insta.png" alt="instagram"></a>
+                <a href="#" title="telegram"><img src="telegram.png" alt="telegram"></a>
+                <a href="#" title="email"><img src="email.png" alt="email"></a>
+                <a href="#" title="linkedin"><img src="linkedin.png" alt="linkedin"></a>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Contact section end-->
+
+<!--copyright-->
+<p class="copyright">&copy; 2020 ^_^</p>
+<script src="js/jquery.min.js"></script>
+<script src="js/main.js"></script>
+
+<!--Your Website is COMPLETE!!!!!!!!!!!!!!-->
+
+</body>
+</html>
